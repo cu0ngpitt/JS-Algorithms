c60f02ac2cc1a6be024279dcc6862ea49a7b3f52
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-08-05 10:08:15 -0500

    Refactored coding for cleaner log

--- ConfirmEnding.js
+++ ConfirmEnding.js
@@ -1,18 +1,21 @@
 function confirmEnding(str, target) {
+  console.log("Original string  : " + str);
+  console.log("Does it end with : " + target);
+
   var str = str.split('').reverse().join('');
   var target = target.split('').reverse().join('');
-  console.log(str);
-  console.log(target);
-
+  
   for(var i = 0; i < target.length; i++) {
     if(target.charAt(i) === str.charAt(i)) {
-      console.log('true');
     } else {
-      console.log('false');
+      console.log(false + '\n');
       return false;
     }
-  } return true;
+  } console.log(true + '\n')
+    return true;
 }
 
 confirmEnding("Bastian", "tian");
 confirmEnding("Congratulation", "on");
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");
+confirmEnding("Open sesame", "pen");
--- Factorize.js
+++ Factorize.js
@@ -1,14 +1,16 @@
 function factorialize(num) {
   if (num === 0) {
-    num = 1;
-    console.log(num);
-    return num;
+    console.log(1);
+    return num = 1;
   }
   for (var i = num; i > 1; i--) {
     num *= (i - 1);
   }
-  return num;
   console.log(num);
+  return num;
 }
 
+factorialize(5);
+factorialize(10);
+factorialize(20);
 factorialize(0);
