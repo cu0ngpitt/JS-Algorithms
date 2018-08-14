b5be8b0a71b61210e4996a2378dddbfb470e3062
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-08-14 12:12:15 -0500

    Split the first array into smaller arrays with lengths given by the 2nd argument

---
+++ ChunkyMonkey.js
@@ -0,0 +1,20 @@
+function chunkArrayInGroups(arr, size) {
+  let newArr = [],
+      j = size;
+
+  for(var i = 0; i < arr.length; i) {
+    newArr.push(arr.slice(i, j));
+    i += size;
+    j += size;
+  }
+  console.log(newArr);
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
